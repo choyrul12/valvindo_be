653686fd39d088795908d14443652f0aec5aedf8
Choyrul A <choy.annam@gmail.com>
2023-10-07 14:44:54 +0700

    initial

--- product/views.py
+++ product/views.py
@@ -8,32 +8,6 @@ from rest_framework import status
 from rest_framework.views import APIView
 from django.db.models import F
 
-class product_category(APIView):
-    def get(self, request):
-        objCategory = productCategory.objects.all()
-        category = CategorySerializer(objCategory, many=True)
-        objProduct = productList.objects.all()
-        product = ProductSerializer(objProduct, many=True)
-
-        result = []
-        for item in category.data:
-            list_product = {
-                "id": item["id"],
-                "category": item["category"],
-                "products": []
-            }
-
-            for detail in product.data:
-                if detail["category"] == item["id"]:
-                    list_product["products"].append({
-                        "id": detail["id"],
-                        "name": detail["name"],
-                        "images": detail["images"],
-                    })
-
-            result.append(list_product)
-        return Response(result)
-
 class product_list(APIView):
     def get(self, request):
         objCategory = productCategory.objects.all()
